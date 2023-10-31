cd156fbb0eb325f72bab73dc206624509fdc86e1
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-31 14:35:29 +0100

    Scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import Grid from "./components/Grid";
 function App() {
 
   useEffect(() => {
+    const root = document.getElementById('root')
+    const d2 = document.getElementById('d2')
+    root.style.height = window.innerHeight + d2.clientHeight + 'px'
+  }, [])
+
+  useEffect(() => {
     const onScroll = e => {
       const d1 = document.getElementById('d1')
       const d2 = document.getElementById('d2')
+      const d3 = document.getElementById('d3')
 
-      d2.style.height = window.scrollY + 'px'
-      d1.style.height = window.innerHeight - d2.clientHeight + 'px'
+      d1.style.height = window.innerHeight - window.scrollY < 0 ? 0 : window.innerHeight - window.scrollY + 'px'
+      d2.style.top = d1.clientHeight + 'px'
+      d3.style.height = window.scrollY - d2.clientHeight + 'px'
     }
 
     window.addEventListener("scroll", onScroll)
@@ -23,6 +31,9 @@ function App() {
     </div>
     <div id="d2" className="part">
       <Grid />
+    </div>
+    <div id="d3" className="part">
+      div3
     </div>
   </>
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,10 +2,18 @@ export default function Grid () {
 
     return <div className="grid">
         <div className="cell">1</div>
-        <div className="cell">2</div>
+        <div className="cell">
+            <span className="data">01/01/2001</span>
+            <span className="name">Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+            <span className="details">en savoir plus</span>
+        </div>
         <div className="cell">3</div>
         <div className="cell">4</div>
-        <div className="cell">5</div>
+        <div className="cell">
+            <span className="data">01/01/2001</span>
+            <span className="name">Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+            <span className="details">en savoir plus</span>
+        </div>
         <div className="cell">6</div>
     </div>
 }
